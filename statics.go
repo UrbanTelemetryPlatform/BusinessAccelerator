636f554d6de88a818c52de7aa7f7ae3c1f81232c
Michael Dietz <michael.dtz@gmail.com>
2018-03-19 08:39:49 +0100

    Change to calculator

--- statics.go
+++ statics.go
@@ -6,15 +6,13 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
 	"google.golang.org/appengine"
 )
 
 func main() {
 	// Serve static files from "static" directory.
 	http.Handle("/", http.FileServer(http.Dir("./ui/")))
-
-	fmt.Println("server is starting up now")
 	appengine.Main()
 }
